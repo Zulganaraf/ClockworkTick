f234bccf090094c6c00c43c1ea58f59d90f4f2e1
Zulganaraf <35299053+Zulganaraf@users.noreply.github.com>
2026-05-22 20:06:55 -0400

    trivial gtest added

---
+++ tests/clockwork_tests.cc
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+// A trivial test to check basic equality
+TEST(TrivialTest, SimpleMath) {
+    EXPECT_EQ(1 + 1, 2);
+}
+
+// A trivial test demonstrating how EXPECT failures work
+TEST(TrivialTest, StringCheck) {
+    EXPECT_STRNE("hello", "world");
+}
